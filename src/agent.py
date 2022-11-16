e743b94d890705b0187d10ba5263239ae07010ed
Nicholas Brown <countdooku.steak@gmail.com>
2022-11-16 13:07:11 -0800

    bot with blacklisted addresses

--- src/agent.py
+++ src/agent.py
@@ -1,33 +1,20 @@
 from forta_agent import Finding, FindingType, FindingSeverity
 
-ERC20_TRANSFER_EVENT = '{"name":"Transfer","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}]}'
-TETHER_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
-TETHER_DECIMALS = 6
-
 #Update this to be accurate
-HIGH_GAS_PRICE = 13080000000
-findings_count = 0
-
+BLACKLISTED_ADDRESSES = ["0x5c1fe6f340dd36b5daf88c2cf390bf715d2af139"]
 
 def handle_transaction(transaction_event):
     findings = []
-
-    # limiting this agent to emit only 5 findings so that the alert feed is not spammed
-    global findings_count
-    if findings_count >= 5:
-        return findings
-    gas_price = transaction_event.gas_price
-    if gas_price > HIGH_GAS_PRICE:
+    from_ = transaction_event.from_
+    if from_ in BLACKLISTED_ADDRESSES:
         findings.append(Finding({
-            'name': 'High Gas Cost Transaction',
-            'description': f'High Gas cost {gas_price / 10**9}',
+            'name': 'Suspicious Transaction',
+            'description': f'Transaction from blacklisted address {from_}',
             'alert_id': 'FORTA-1',
-            'severity': FindingSeverity.Low,
+            'severity': FindingSeverity.High,
             'type': FindingType.Info,
             'metadata': {
-                'from_': transaction_event.from_,
+                'from_': from_,
             }
         }))
-        findings_count += 1
-# 
     return findings
