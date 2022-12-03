14a532a6d236aa014155fec4177e59ac04c29269
Nicholas Brown <countdooku.steak@gmail.com>
2022-12-03 12:23:47 -0800

    update bot to check if to is in blacklist as well

--- src/agent.py
+++ src/agent.py
@@ -1,20 +1,29 @@
 from forta_agent import Finding, FindingType, FindingSeverity
 
 #Update this to be accurate
-BLACKLISTED_ADDRESSES = ["0x5c1fe6f340dd36b5daf88c2cf390bf715d2af139","0x15FE7CE21B181a4706063B31E5c968477c7A00Cc"]
+BLACKLISTED_ADDRESSES = ["0x5c1fe6f340dd36b5daf88c2cf390bf715d2af139","0x15fe7ce21b181a4706063b31e5c968477c7a00cc", "0xb95f4d5529e3adcc10447d7e6797424edd1c701d"]
 
 def handle_transaction(transaction_event):
     findings = []
     from_ = transaction_event.from_
-    if from_ in BLACKLISTED_ADDRESSES:
+    to = transaction_event.to
+    alert = False
+    addr = ""
+    if (from_ in BLACKLISTED_ADDRESSES):
+        alert = True
+        addr = from_
+    elif (to in BLACKLISTED_ADDRESSES):
+        alert = True
+        addr = to
+    if alert:
         findings.append(Finding({
             'name': 'Suspicious Transaction',
-            'description': f'Transaction from blacklisted address {from_}',
+            'description': f'Transaction involving blacklisted address {addr}',
             'alert_id': 'FORTAKNIGHT-1',
             'severity': FindingSeverity.High,
             'type': FindingType.Info,
             'metadata': {
-                'from': from_,
+                'addr': addr,
             }
         }))
     return findings
--- src/agent_test.py
+++ src/agent_test.py
@@ -2,28 +2,43 @@ from unittest.mock import Mock
 from forta_agent import FindingSeverity, FindingType, create_transaction_event
 from agent import handle_transaction, BLACKLISTED_ADDRESSES
 
-VALID_ADDRESS = "0x58f3DB9d89eCC7c653F531689BCceD3937b797cc"
+VALID_ADDRESS1 = "0x58f3DB9d89eCC7c653F531689BCceD3937b797cc"
+VALID_ADDRESS2 = "0x58f3DB9d89eCC7c653F531689BCceD3937b797cd"
 
 
 
 class TestHighTetherTransferAgent:
-    def test_returns_empty_findings_if_no_tether_transfers(self):
-        mock_tx_event = create_transaction_event({"transaction": {"from": VALID_ADDRESS}})
+    def test_returns_empty_findings_if_from_and_to_not_in_blacklist(self):
+        mock_tx_event = create_transaction_event({"transaction": {"from": VALID_ADDRESS1, "to": VALID_ADDRESS2}})
 
         findings = handle_transaction(mock_tx_event)
 
         assert len(findings) == 0
 
-    def test_returns_finding_if_tether_transfer_over_10k(self):
-        mock_tx_event = create_transaction_event({"transaction": {"from": BLACKLISTED_ADDRESSES[0]}})
+    def test_returns_finding_if_from_in_blacklist(self):
+        mock_tx_event = create_transaction_event({"transaction": {"from": BLACKLISTED_ADDRESSES[0], "to": VALID_ADDRESS1}})
 
         findings = handle_transaction(mock_tx_event)
 
         assert len(findings) == 1
         finding = findings[0]
         assert finding.name == "Suspicious Transaction"
-        assert finding.description == f'Transaction from blacklisted address {BLACKLISTED_ADDRESSES[0]}'
+        assert finding.description == f'Transaction involving blacklisted address {BLACKLISTED_ADDRESSES[0]}'
         assert finding.alert_id == "FORTAKNIGHT-1"
         assert finding.severity == FindingSeverity.High
         assert finding.type == FindingType.Info
-        assert finding.metadata['from'] == BLACKLISTED_ADDRESSES[0]
+        assert finding.metadata['addr'] == BLACKLISTED_ADDRESSES[0]
+
+    def test_returns_finding_if_to_in_blacklist(self):
+        mock_tx_event = create_transaction_event({"transaction": {"from": VALID_ADDRESS1, "to": BLACKLISTED_ADDRESSES[0]}})
+
+        findings = handle_transaction(mock_tx_event)
+
+        assert len(findings) == 1
+        finding = findings[0]
+        assert finding.name == "Suspicious Transaction"
+        assert finding.description == f'Transaction involving blacklisted address {BLACKLISTED_ADDRESSES[0]}'
+        assert finding.alert_id == "FORTAKNIGHT-1"
+        assert finding.severity == FindingSeverity.High
+        assert finding.type == FindingType.Info
+        assert finding.metadata['addr'] == BLACKLISTED_ADDRESSES[0]
