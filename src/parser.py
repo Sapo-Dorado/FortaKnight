fc7d4559a4e618321773871a6f79062fae92b6fd
Nicholas Brown <countdooku.steak@gmail.com>
2023-01-17 19:45:47 -0800

    add parser

---
+++ src/parser.py
@@ -0,0 +1,22 @@
+from solidity_parser import parser
+
+def getContracts(root):
+  contracts = []
+  for n in root.children:
+    if(n.type == "ContractDefinition"):
+      contracts.append(n)
+  return contracts
+
+def getFunctions(contract):
+  functions = []
+  for v in contract.subNodes:
+    if(v.type == "FunctionDefinition"):
+      functions.append(v)
+  return functions
+
+def parse(file):
+  return parser.parse_file(file, loc=False)
+
+
+ast = parse("./test/contracts/TetherToken.sol")
+print(getFunctions(getContracts(ast)[0])[0])
---
+++ test/parser_test.py
@@ -0,0 +1,27 @@
+import src.parser as parser
+
+CONTRACTS = ["SafeMath", "Ownable","Pausable", "ERC20Basic", "ERC20","BasicToken","StandardToken","UpgradedStandardToken","TetherToken"]
+TETHER_FUNCTIONS = ["TetherToken", "transfer", "transferFrom", "balanceOf", "approve", "allowance", "deprecate", "totalSupply", "issue", "redeem", "setParams"]
+
+class TestParser:
+  def test_parse_parses_contracts(self):
+    ast = parser.parse("./test/contracts/TetherToken.sol")
+    contracts = parser.getContracts(ast)
+    names = [c.name for c in contracts]
+    for contract_name in CONTRACTS:
+      assert(contract_name in names)
+
+  def test_parse_parses_functions(self):
+    ast = parser.parse("./test/contracts/TetherToken.sol")
+    contracts = parser.getContracts(ast)
+    contract = None
+    for c in contracts:
+      if c.name == "TetherToken":
+        contract = c
+        break
+    functions = parser.getFunctions(contract)
+    names = [f.name for f in functions]
+    for fn_name in TETHER_FUNCTIONS:
+      assert(fn_name in names)
+
+
