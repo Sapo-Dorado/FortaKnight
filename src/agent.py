94ebe14848e5f1d14482feb6193db74a04bc8c04
Nicholas Brown <countdooku.steak@gmail.com>
2023-02-28 18:34:22 -0800

    update bot to use static analysis

--- src/agent.py
+++ src/agent.py
@@ -1,29 +1,17 @@
 from forta_agent import Finding, FindingType, FindingSeverity
-
-#Update this to be accurate
-BLACKLISTED_ADDRESSES = ["0x5c1fe6f340dd36b5daf88c2cf390bf715d2af139","0x15fe7ce21b181a4706063b31e5c968477c7a00cc", "0xb95f4d5529e3adcc10447d7e6797424edd1c701d"]
+from src.detectors import processContract
 
 def handle_transaction(transaction_event):
     findings = []
-    from_ = transaction_event.from_
-    to = transaction_event.to
-    alert = False
-    addr = ""
-    if (from_ in BLACKLISTED_ADDRESSES):
-        alert = True
-        addr = from_
-    elif (to in BLACKLISTED_ADDRESSES):
-        alert = True
-        addr = to
-    if alert:
-        findings.append(Finding({
-            'name': 'Suspicious Transaction',
-            'description': f'Transaction involving blacklisted address {addr}',
-            'alert_id': 'FORTAKNIGHT-1',
-            'severity': FindingSeverity.High,
-            'type': FindingType.Info,
-            'metadata': {
-                'addr': addr,
-            }
-        }))
+    for address in transaction_event.addresses:
+        for description in processContract(address):
+            findings.append(Finding({
+                'name': 'Suspicious Contract Code',
+                'description': description,
+                'alert_id': 'FORTAKNIGHT-1',
+                'severity': FindingSeverity.Low,
+                'type': FindingType.Info,
+                'addresses': [address, transaction_event.from_],
+                'metadata': {}
+            }))
     return findings
