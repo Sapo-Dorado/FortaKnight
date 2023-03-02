2d8429fc7227c301b638d27a0781e7e274fd1282
Alejandro Rojas Rodriguez <arojasrodriguez@ucsb.edulscdgit config --global user.email arojasrodriguez@ucsb.edu>
2023-03-01 19:54:08 -0800

    Created analysis for the testing samples and verify the info spread cheat

--- analysis/Testing_Samples/contract_65.sol
+++ analysis/Testing_Samples/contract_65.sol
@@ -1,15 +1,13 @@
 pragma solidity 0.4.24;
 
-
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
 library SafeMath {
-
     /**
-    * @dev Multiplies two numbers, throws on overflow.
-    */
+     * @dev Multiplies two numbers, throws on overflow.
+     */
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
             return 0;
@@ -20,8 +18,8 @@ library SafeMath {
     }
 
     /**
-    * @dev Integer division of two numbers, truncating the quotient.
-    */
+     * @dev Integer division of two numbers, truncating the quotient.
+     */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         // assert(b > 0); // Solidity automatically throws when dividing by 0
         uint256 c = a / b;
@@ -30,16 +28,16 @@ library SafeMath {
     }
 
     /**
-    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-    */
+     * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+     */
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         assert(b <= a);
         return a - b;
     }
 
     /**
-    * @dev Adds two numbers, throws on overflow.
-    */
+     * @dev Adds two numbers, throws on overflow.
+     */
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         assert(c >= a);
@@ -54,8 +52,11 @@ library SafeMath {
  */
 contract ERC20Basic {
     function totalSupply() public view returns (uint256);
+
     function balanceOf(address who) public view returns (uint256);
+
     function transfer(address to, uint256 value) public returns (bool);
+
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
@@ -71,17 +72,17 @@ contract BasicToken is ERC20Basic {
     uint256 totalSupply_;
 
     /**
-    * @dev total number of tokens in existence
-    */
+     * @dev total number of tokens in existence
+     */
     function totalSupply() public view returns (uint256) {
         return totalSupply_;
     }
 
     /**
-    * @dev transfer token for a specified address
-    * @param _to The address to transfer to.
-    * @param _value The amount to be transferred.
-    */
+     * @dev transfer token for a specified address
+     * @param _to The address to transfer to.
+     * @param _value The amount to be transferred.
+     */
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(_to != address(0));
         require(_value <= balances[msg.sender]);
@@ -94,10 +95,10 @@ contract BasicToken is ERC20Basic {
     }
 
     /**
-    * @dev Gets the balance of the specified address.
-    * @param _owner The address to query the the balance of.
-    * @return An uint256 representing the amount owned by the passed address.
-    */
+     * @dev Gets the balance of the specified address.
+     * @param _owner The address to query the the balance of.
+     * @return An uint256 representing the amount owned by the passed address.
+     */
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
@@ -108,10 +109,24 @@ contract BasicToken is ERC20Basic {
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
 contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) public view returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function allowance(
+        address owner,
+        address spender
+    ) public view returns (uint256);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) public returns (bool);
+
     function approve(address spender, uint256 value) public returns (bool);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 }
 
 /**
@@ -122,8 +137,7 @@ contract ERC20 is ERC20Basic {
  * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
  */
 contract StandardToken is ERC20, BasicToken {
-
-    mapping (address => mapping (address => uint256)) internal allowed;
+    mapping(address => mapping(address => uint256)) internal allowed;
 
     /**
      * @dev Transfer tokens from one address to another
@@ -131,7 +145,11 @@ contract StandardToken is ERC20, BasicToken {
      * @param _to address The address which you want to transfer to
      * @param _value uint256 the amount of tokens to be transferred
      */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool) {
         require(_to != address(0));
         require(_value <= balances[_from]);
         require(_value <= allowed[_from][msg.sender]);
@@ -165,7 +183,10 @@ contract StandardToken is ERC20, BasicToken {
      * @param _spender address The address which will spend the funds.
      * @return A uint256 specifying the amount of tokens still available for the spender.
      */
-    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+    function allowance(
+        address _owner,
+        address _spender
+    ) public constant returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
 
@@ -175,13 +196,21 @@ contract StandardToken is ERC20, BasicToken {
      * the first transaction is mined)
      * From MonolithDAO Token.sol
      */
-    function increaseApproval (address _spender, uint _addedValue) public returns (bool success) {
-        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    function increaseApproval(
+        address _spender,
+        uint _addedValue
+    ) public returns (bool success) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(
+            _addedValue
+        );
         emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
 
-    function decreaseApproval (address _spender, uint _subtractedValue) public returns (bool success) {
+    function decreaseApproval(
+        address _spender,
+        uint _subtractedValue
+    ) public returns (bool success) {
         uint oldValue = allowed[msg.sender][_spender];
         if (_subtractedValue > oldValue) {
             allowed[msg.sender][_spender] = 0;
@@ -200,11 +229,20 @@ contract StandardToken is ERC20, BasicToken {
  * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
  */
 library SafeERC20 {
-    function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+    function safeTransfer(
+        ERC20Basic token,
+        address to,
+        uint256 value
+    ) internal {
         assert(token.transfer(to, value));
     }
 
-    function safeTransferFrom(ERC20 token, address from, address to, uint256 value) internal {
+    function safeTransferFrom(
+        ERC20 token,
+        address from,
+        address to,
+        uint256 value
+    ) internal {
         assert(token.transferFrom(from, to, value));
     }
 
@@ -220,7 +258,7 @@ contract Owned {
         owner = msg.sender;
     }
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
@@ -248,8 +286,8 @@ contract TokenVesting is Owned {
 
     bool public revocable;
 
-    mapping (address => uint256) public released;
-    mapping (address => bool) public revoked;
+    mapping(address => uint256) public released;
+    mapping(address => bool) public revoked;
 
     address internal ownerShip;
 
@@ -270,9 +308,7 @@ contract TokenVesting is Owned {
         uint256 _duration,
         bool _revocable,
         address _realOwner
-    )
-        public
-    {
+    ) public {
         require(_beneficiary != address(0));
         require(_cliff <= _duration);
 
@@ -379,14 +415,14 @@ contract TokenVault {
  * @dev Token that can be irreversibly burned (destroyed).
  */
 contract BurnableToken is StandardToken {
-
     event Burn(address indexed burner, uint256 value);
 
     /**
      * @dev Burns a specific amount of tokens.
      * @param _value The amount of token to be burned.
      */
-    function burn(uint256 _value) public { //Burn function
+    function burn(uint256 _value) public {
+        //Burn function
         require(_value > 0);
         require(_value <= balances[msg.sender]);
         // no need to require value <= totalSupply, since that would imply the
@@ -404,7 +440,7 @@ contract CES_Token is BurnableToken, Owned {
     uint8 public constant decimals = 18;
 
     /// Maximum tokens to be allocated ( 20.0 billion BKB)
-    uint256 public constant HARD_CAP = 20000000000 * 10**uint256(decimals);
+    uint256 public constant HARD_CAP = 20000000000 * 10 ** uint256(decimals);
 
     /// This address will be used to distribute the team, advisors and reserve tokens
     address public saleTokensAddress;
@@ -413,10 +449,10 @@ contract CES_Token is BurnableToken, Owned {
     TokenVault public reserveTokensVault;
 
     /// Date when the vesting for regular users starts
-    uint64 public daySecond     = 86400;
-    uint64 public lock90Days    = 90;
+    uint64 public daySecond = 86400;
+    uint64 public lock90Days = 90;
     uint64 public unlock100Days = 100;
-    uint64 public lock365Days   = 365;
+    uint64 public lock365Days = 365;
 
     /// Store the vesting contract addresses for each sale contributor
     mapping(address => address) public vestingOf;
@@ -433,7 +469,7 @@ contract CES_Token is BurnableToken, Owned {
         require(totalSupply_ <= HARD_CAP);
     }
 
-    /// @dev Create a ReserveTokenVault 
+    /// @dev Create a ReserveTokenVault
     function createReserveTokensVault() external onlyOwner {
         require(reserveTokensVault == address(0));
 
@@ -445,7 +481,9 @@ contract CES_Token is BurnableToken, Owned {
     }
 
     /// @dev Create a TokenVault and fill with the specified newly minted tokens
-    function createTokenVaultInt(uint256 tokens) internal onlyOwner returns (TokenVault) {
+    function createTokenVaultInt(
+        uint256 tokens
+    ) internal onlyOwner returns (TokenVault) {
         TokenVault tokenVault = new TokenVault(ERC20(this));
         createTokensInt(tokens, tokenVault);
         tokenVault.fillUpAllowance();
@@ -453,8 +491,12 @@ contract CES_Token is BurnableToken, Owned {
     }
 
     // @dev create specified number of tokens and transfer to destination
-    function createTokensInt(uint256 _tokens, address _destination) internal onlyOwner { //MINT FUNCTION
-        uint256 tokens = _tokens * 10**uint256(decimals);
+    function createTokensInt(
+        uint256 _tokens,
+        address _destination
+    ) internal onlyOwner {
+        //MINT FUNCTION
+        uint256 tokens = _tokens * 10 ** uint256(decimals);
         totalSupply_ = totalSupply_.add(tokens);
         balances[_destination] = balances[_destination].add(tokens);
         emit Transfer(0x0, _destination, tokens);
@@ -464,64 +506,133 @@ contract CES_Token is BurnableToken, Owned {
 
     /// @dev vest Detail : second unit
     function vestTokensDetailInt(
-                        address _beneficiary,
-                        uint256 _startS,
-                        uint256 _cliffS,
-                        uint256 _durationS,
-                        bool _revocable,
-                        uint256 _tokensAmountInt) external onlyOwner {
+        address _beneficiary,
+        uint256 _startS,
+        uint256 _cliffS,
+        uint256 _durationS,
+        bool _revocable,
+        uint256 _tokensAmountInt
+    ) external onlyOwner {
         require(_beneficiary != address(0));
 
-        uint256 tokensAmount = _tokensAmountInt * 10**uint256(decimals);
+        uint256 tokensAmount = _tokensAmountInt * 10 ** uint256(decimals);
 
-        if(vestingOf[_beneficiary] == 0x0) {
-            TokenVesting vesting = new TokenVesting(_beneficiary, _startS, _cliffS, _durationS, _revocable, owner);
+        if (vestingOf[_beneficiary] == 0x0) {
+            TokenVesting vesting = new TokenVesting(
+                _beneficiary,
+                _startS,
+                _cliffS,
+                _durationS,
+                _revocable,
+                owner
+            );
             vestingOf[_beneficiary] = address(vesting);
         }
 
-        require(this.transferFrom(reserveTokensVault, vestingOf[_beneficiary], tokensAmount));
+        require(
+            this.transferFrom(
+                reserveTokensVault,
+                vestingOf[_beneficiary],
+                tokensAmount
+            )
+        );
     }
 
     /// @dev vest StartAt : day unit
     function vestTokensStartAtInt(
-                            address _beneficiary, 
-                            uint256 _tokensAmountInt,
-                            uint256 _startS,
-                            uint256 _afterDay,
-                            uint256 _cliffDay,
-                            uint256 _durationDay ) public onlyOwner {
+        address _beneficiary,
+        uint256 _tokensAmountInt,
+        uint256 _startS,
+        uint256 _afterDay,
+        uint256 _cliffDay,
+        uint256 _durationDay
+    ) public onlyOwner {
         require(_beneficiary != address(0));
 
-        uint256 tokensAmount = _tokensAmountInt * 10**uint256(decimals);
+        uint256 tokensAmount = _tokensAmountInt * 10 ** uint256(decimals);
         uint256 afterSec = _afterDay * daySecond;
         uint256 cliffSec = _cliffDay * daySecond;
         uint256 durationSec = _durationDay * daySecond;
 
-        if(vestingOf[_beneficiary] == 0x0) {
-            TokenVesting vesting = new TokenVesting(_beneficiary, _startS + afterSec, cliffSec, durationSec, true, owner);
+        if (vestingOf[_beneficiary] == 0x0) {
+            TokenVesting vesting = new TokenVesting(
+                _beneficiary,
+                _startS + afterSec,
+                cliffSec,
+                durationSec,
+                true,
+                owner
+            );
             vestingOf[_beneficiary] = address(vesting);
         }
 
-        require(this.transferFrom(reserveTokensVault, vestingOf[_beneficiary], tokensAmount));
+        require(
+            this.transferFrom(
+                reserveTokensVault,
+                vestingOf[_beneficiary],
+                tokensAmount
+            )
+        );
     }
 
     /// @dev vest function from now
-    function vestTokensFromNowInt(address _beneficiary, uint256 _tokensAmountInt, uint256 _afterDay, uint256 _cliffDay, uint256 _durationDay ) public onlyOwner {
-        vestTokensStartAtInt(_beneficiary, _tokensAmountInt, now, _afterDay, _cliffDay, _durationDay);
+    function vestTokensFromNowInt(
+        address _beneficiary,
+        uint256 _tokensAmountInt,
+        uint256 _afterDay,
+        uint256 _cliffDay,
+        uint256 _durationDay
+    ) public onlyOwner {
+        vestTokensStartAtInt(
+            _beneficiary,
+            _tokensAmountInt,
+            now,
+            _afterDay,
+            _cliffDay,
+            _durationDay
+        );
     }
 
-    /// @dev vest the sale contributor tokens for 100 days, 1% gradual release 
-    function vestCmdNow1PercentInt(address _beneficiary, uint256 _tokensAmountInt) external onlyOwner {
-        vestTokensFromNowInt(_beneficiary, _tokensAmountInt, 0, 0, unlock100Days);
+    /// @dev vest the sale contributor tokens for 100 days, 1% gradual release
+    function vestCmdNow1PercentInt(
+        address _beneficiary,
+        uint256 _tokensAmountInt
+    ) external onlyOwner {
+        vestTokensFromNowInt(
+            _beneficiary,
+            _tokensAmountInt,
+            0,
+            0,
+            unlock100Days
+        );
     }
+
     /// @dev vest the sale contributor tokens for 100 days, 1% gradual release after 3 month later, no cliff
-    function vestCmd3Month1PercentInt(address _beneficiary, uint256 _tokensAmountInt) external onlyOwner {
-        vestTokensFromNowInt(_beneficiary, _tokensAmountInt, lock90Days, 0, unlock100Days);
+    function vestCmd3Month1PercentInt(
+        address _beneficiary,
+        uint256 _tokensAmountInt
+    ) external onlyOwner {
+        vestTokensFromNowInt(
+            _beneficiary,
+            _tokensAmountInt,
+            lock90Days,
+            0,
+            unlock100Days
+        );
     }
 
     /// @dev vest the sale contributor tokens 100% release after 1 year
-    function vestCmd1YearInstantInt(address _beneficiary, uint256 _tokensAmountInt) external onlyOwner {
-        vestTokensFromNowInt(_beneficiary, _tokensAmountInt, 0, lock365Days, lock365Days);
+    function vestCmd1YearInstantInt(
+        address _beneficiary,
+        uint256 _tokensAmountInt
+    ) external onlyOwner {
+        vestTokensFromNowInt(
+            _beneficiary,
+            _tokensAmountInt,
+            0,
+            lock365Days,
+            lock365Days
+        );
     }
 
     /// @dev releases vested tokens for the caller's own address
@@ -541,8 +652,10 @@ contract CES_Token is BurnableToken, Owned {
     }
 
     /// @dev check the locked but releaseable balance of an owner
-    function releaseableBalanceOf(address _owner) public view returns (uint256) {
-        if (vestingOf[_owner] == address(0) ) {
+    function releaseableBalanceOf(
+        address _owner
+    ) public view returns (uint256) {
+        if (vestingOf[_owner] == address(0)) {
             return 0;
         } else {
             return TokenVesting(vestingOf[_owner]).releasableAmount(this);
@@ -555,10 +668,9 @@ contract CES_Token is BurnableToken, Owned {
         TokenVesting(vestingOf[_owner]).revoke(this);
     }
 
-    /// @dev Create a ReserveTokenVault 
+    /// @dev Create a ReserveTokenVault
     function makeReserveToVault() external onlyOwner {
         require(reserveTokensVault != address(0));
         reserveTokensVault.fillUpAllowance();
     }
-
 }
